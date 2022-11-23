9ed1884a8b3effb56f6ea334bc68354bbbeec302
Maciej Kudela <56248505+mackudela@users.noreply.github.com>
2022-11-23 14:32:06 +0100

    Initialization of backend project

---
+++ MovieCollectionManagement/MovieCollectionManagement/src/main/java/pl/polsl/moviecollectionmanagement/MovieCollectionManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.moviecollectionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCollectionManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCollectionManagementApplication.class, args);
+    }
+
+}
